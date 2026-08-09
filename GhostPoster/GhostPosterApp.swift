ae9a9e85a806db911156ccf0c77f2f505f14ea39
Susie8FA7 <starship.sagittarius+github@gmail.com>
2026-08-09 10:09:30 +0900

    Initial Commit

---
+++ GhostPoster/GhostPosterApp.swift
@@ -0,0 +1,16 @@
+//
+//  GhostPosterApp.swift
+//  GhostPoster
+//
+//
+
+import SwiftUI
+
+@main
+struct GhostPosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GhostPosterTests/GhostPosterTests.swift
@@ -0,0 +1,18 @@
+//
+//  GhostPosterTests.swift
+//  GhostPosterTests
+//
+//
+
+import Testing
+@testable import GhostPoster
+
+struct GhostPosterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
